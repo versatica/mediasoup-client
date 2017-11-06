2dd64a07b51a0554c7853634d2991e926da6aeb7
Iñaki Baz Castillo <ibc@aliax.net>
2017-11-06 01:19:00 +0100

    2.0.2: fix bower.json

--- dist/mediasoup-client.js
+++ dist/mediasoup-client.js
@@ -1,5 +1,5 @@
 /*
- * mediasoup-client v2.0.1
+ * mediasoup-client v2.0.2
  * mediasoup client side JavaScript SDK
  * https://mediasoup.org
  * Copyright © 2017, Iñaki Baz Castillo <ibc@aliax.net>
