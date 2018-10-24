6b036f9efc71b4c6006ca9cb38553383d142dbb4
imosquera <imosquera@tuenti.com>
2018-10-24 22:23:44 +0200

    a jest testing poc

---
+++ __tests__/Room.test.js
@@ -0,0 +1,65 @@
+const mediasoupClient = require('../lib/index')
+const { TimeoutError } = require('../lib/errors')
+
+const ANY_ROOM_ID = 1;
+const ANY_PRODUCER_ID = 1;
+const ANY_PEER_NAME = 'me';
+
+
+let room;
+
+beforeEach(() => {
+    room = new mediasoupClient.Room();
+});
+
+test('Room can be instantiated', () =>
+{
+	expect(room).toBeDefined();
+});
+
+test('Initial state', () =>
+{
+    expect(room.joined).toEqual(false);
+    expect(room.closed).toEqual(false);
+    expect(room.peerName).toBeNull();
+    expect(room.transports).toEqual([]);
+    expect(room.producers).toEqual([]);
+    expect(room.peers).toEqual([]);
+});
+
+test('getTransportById if not found', () =>
+{
+    room = new mediasoupClient.Room();
+
+    expect(room.getTransportById(ANY_ROOM_ID)).toBeUndefined();
+});
+
+test('getProducerById if not found', () =>
+{
+    room = new mediasoupClient.Room();
+
+    expect(room.getProducerById(ANY_PRODUCER_ID)).toBeUndefined();
+});
+
+test('getProducerById if not found', () =>
+{
+    room = new mediasoupClient.Room();
+
+    expect(room.getPeerByName(ANY_PEER_NAME)).toBeUndefined();
+});
+
+test('join with non string peerName', () =>
+{
+    room = new mediasoupClient.Room();
+    return expect(room.join(1, {})).rejects.toThrow(TypeError);
+});
+
+test('join with string peerName rejecting with timeout', () =>
+{
+    room = new mediasoupClient.Room({ requestTimeout: 100 });
+    return expect(room.join(ANY_PEER_NAME, {})).rejects.toEqual(new TimeoutError('timeout'))
+});
+
+
+
+
