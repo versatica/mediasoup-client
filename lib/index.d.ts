7979926723282dd0462c568332ddaafcd3b7ce28
Iñaki Baz Castillo <ibc@aliax.net>
2021-04-19 17:50:04 +0200

    3.6.30

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,7 +7,7 @@ export { types };
 /**
  * Expose mediasoup-client version.
  */
-export declare const version = "3.6.29";
+export declare const version = "3.6.30";
 /**
  * Expose Device class and detectDevice() helper.
  */
