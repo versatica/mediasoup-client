05af1fedc5aafd300818cb6cfe6e105c9784cfdf
José Luis Millán <jmillan@aliax.net>
2021-06-24 12:21:53 +0200

    3.6.34

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,7 +7,7 @@ export { types };
 /**
  * Expose mediasoup-client version.
  */
-export declare const version = "3.6.33";
+export declare const version = "3.6.34";
 /**
  * Expose Device class and detectDevice() helper.
  */
