779e8e34740d9295700997373bed31006cad235e
Iñaki Baz Castillo <ibc@aliax.net>
2022-11-18 14:14:26 +0100

    do remove lib/ from repo

--- lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Clones the given data.
- */
-export declare function clone(data: any, defaultValue: any): any;
-/**
- * Generates a random positive integer.
- */
-export declare function generateRandomNumber(): number;
-//# sourceMappingURL=utils.d.ts.map
