d03e9ce8417314cc68da4a094d063654d1346089
Iñaki Baz Castillo <ibc@aliax.net>
2023-07-13 17:02:40 +0200

    Revert export only TS types in types.ts
    This requires typescript >= 5 in apps importing mediasoup-client and we don't want to be that strict.
    
    So let's reopen ticket #273.

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
-export type * from './Device';
-export type * from './Transport';
-export type * from './Producer';
-export type * from './Consumer';
-export type * from './DataProducer';
-export type * from './DataConsumer';
-export type * from './RtpParameters';
-export type * from './SctpParameters';
-export type * from './handlers/HandlerInterface';
+export * from './Device';
+export * from './Transport';
+export * from './Producer';
+export * from './Consumer';
+export * from './DataProducer';
+export * from './DataConsumer';
+export * from './RtpParameters';
+export * from './SctpParameters';
+export * from './handlers/HandlerInterface';
 // We cannot export only the type of error classes because those are useless.
 export * from './errors';
 export type { ScalabilityMode } from './scalabilityModes';
