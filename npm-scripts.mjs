bef523fd3b3bec26cab03b5b7c92f3346856564d
Iñaki Baz Castillo <ibc@aliax.net>
2025-07-21 15:51:07 +0200

    improve npm-scripts.mjs

--- npm-scripts.mjs
+++ npm-scripts.mjs
@@ -31,12 +31,12 @@ const PRETTIER_PATHS = [
 ].join(' ');
 
 const task = process.argv[2];
-const args = process.argv.slice(3).join(' ');
+const taskArgs = process.argv.slice(3).join(' ');
 
 run();
 
 async function run() {
-	logInfo(args ? `[args:"${args}"]` : '');
+	logInfo(taskArgs ? `[args:"${taskArgs}"]` : '');
 
 	switch (task) {
 		// As per NPM documentation (https://docs.npmjs.com/cli/v9/using-npm/scripts)
@@ -92,7 +92,7 @@ async function run() {
 
 		case 'coverage': {
 			replaceVersion();
-			executeCmd(`jest --coverage ${args}`);
+			executeCmd(`jest --coverage ${taskArgs}`);
 			executeCmd('open-cli coverage/lcov-report/index.html');
 
 			break;
@@ -110,7 +110,7 @@ async function run() {
 			executeCmd(`git tag -a ${PKG.version} -m '${PKG.version}'`);
 			executeCmd(`git push origin v${MAYOR_VERSION}`);
 			executeCmd(`git push origin '${PKG.version}'`);
-			executeCmd('npm publish');
+			executeInteractiveCmd('npm publish');
 
 			break;
 		}
@@ -165,7 +165,7 @@ function buildTypescript({ force }) {
 	deleteLib();
 
 	// Generate .js CommonJS code and .d.ts TypeScript declaration files in lib/.
-	executeCmd(`tsc ${args}`);
+	executeCmd(`tsc ${taskArgs}`);
 }
 
 function watchTypescript() {
@@ -173,7 +173,7 @@ function watchTypescript() {
 
 	deleteLib();
 
-	executeCmd(`tsc --watch ${args}`);
+	executeCmd(`tsc --watch ${taskArgs}`);
 }
 
 function lint() {
@@ -199,7 +199,7 @@ function format() {
 function test() {
 	logInfo('test()');
 
-	executeCmd(`jest --silent false --detectOpenHandles ${args}`);
+	executeCmd(`jest --silent false --detectOpenHandles ${taskArgs}`);
 }
 
 function installDeps() {
@@ -237,20 +237,32 @@ function executeCmd(command) {
 	}
 }
 
-function logInfo(message) {
+function executeInteractiveCmd(command) {
+	logInfo(`executeInteractiveCmd(): ${command}`);
+
+	try {
+		execSync(command, { stdio: 'inherit', env: process.env });
+	} catch (error) {
+		logError(`executeInteractiveCmd() failed, exiting: ${error}`);
+
+		exitWithError();
+	}
+}
+
+function logInfo(...args) {
 	// eslint-disable-next-line no-console
-	console.log(`npm-scripts \x1b[36m[INFO] [${task}]\x1b[0m`, message);
+	console.log(`npm-scripts.mjs \x1b[36m[INFO] [${task}]\x1b[0m`, ...args);
 }
 
 // eslint-disable-next-line no-unused-vars
-function logWarn(message) {
+function logWarn(...args) {
 	// eslint-disable-next-line no-console
-	console.warn(`npm-scripts \x1b[33m[WARN] [${task}]\x1b[0m`, message);
+	console.warn(`npm-scripts.mjs \x1b[33m[WARN] [${task}]\x1b\0m`, ...args);
 }
 
-function logError(message) {
+function logError(...args) {
 	// eslint-disable-next-line no-console
-	console.error(`npm-scripts \x1b[31m[ERROR] [${task}]\x1b[0m`, message);
+	console.error(`npm-scripts.mjs \x1b[31m[ERROR] [${task}]\x1b[0m`, ...args);
 }
 
 function exitWithError() {
