versatica/mediasoup-client

commit cbf5060dc5c45f94374cd3a1f4b3130f4bdff6b3

Don't expect static codec payload types and RTP extension IDs (#336)
## Details

- Fixes #330
- In Chrome M140 the way codec payload types and RTP extension IDs are assigned in the SDP will change and will break assumptions we do in mediasoup-client.
- Before this PR, mediasoup-client obtains local RTP capabilities when `device.load()` is called and assumes that the codec payload types and RTP extension IDs will be later the same ones when sending `Transports` are created and `Producers` are created on them. So for example, in Chrome M140, if we start with video and then we add audio to a sending `Transport`, those numeric payload types and extension local IDs will be different than the ones in our discovered RTP capabilities, and hence our sending `RtpParameters` will have wrong values that won't match what the browser will really send into the RTP packets, so everything will fail.
- In this PR, mediasoup-client computes local capabilities every time `transport.produce()` is called and obtains the codec payload types and RTP extension IDs dynamically from the latest SDP offer generated by the browser.

## Important

- **Upgrading to next mediasoup-client version will be urgently needed, otherwise many apps will stop working!**

src/Device.ts
@@ -91,13 +91,18 @@ export class Device {
 	private readonly _handlerName: string;
 	// Loaded flag.
 	private _loaded = false;
-	// Extended RTP capabilities.
-	private _extendedRtpCapabilities?: ExtendedRtpCapabilities;
+	// Callback for sending Transports to request sending extended RTP capabilities
+	// on demand.
+	private _getSendExtendedRtpCapabilities?: (
+		nativeRtpCapabilities: RtpCapabilities
+	) => ExtendedRtpCapabilities;
 	// Local RTP capabilities for receiving media.
 	private _recvRtpCapabilities?: RtpCapabilities;
-	// Whether we can produce audio/video based on computed extended RTP
-	// capabilities.
-	private readonly _canProduceByKind: CanProduceByKind;
+	// Whether we can produce audio/video based on remote RTP capabilities.
+	private readonly _canProduceByKind: CanProduceByKind = {
+		audio: false,
+		video: false,
+	};
 	// Local SCTP capabilities.
 	private _sctpCapabilities?: SctpCapabilities;
 	// Observer instance.
@@ -200,13 +205,6 @@ export class Device {
 		}
 
 		this._handlerName = this._handlerFactory.name;
-		this._extendedRtpCapabilities = undefined;
-		this._recvRtpCapabilities = undefined;
-		this._canProduceByKind = {
-			audio: false,
-			video: false,
-		};
-		this._sctpCapabilities = undefined;
 	}
 
 	/**
@@ -280,46 +278,39 @@ export class Device {
 		const { getNativeRtpCapabilities, getNativeSctpCapabilities } =
 			this._handlerFactory;
 
-		const nativeRtpCapabilities = await getNativeRtpCapabilities();
-
-		logger.debug(
-			'load() | got native RTP capabilities:%o',
-			nativeRtpCapabilities
-		);
-
-		// Clone obtained native RTP capabilities to not modify input data.
 		const clonedNativeRtpCapabilities = utils.clone<RtpCapabilities>(
-			nativeRtpCapabilities
+			await getNativeRtpCapabilities()
 		);
 
 		// This may throw.
 		ortc.validateRtpCapabilities(clonedNativeRtpCapabilities);
 
-		// Get extended RTP capabilities.
-		this._extendedRtpCapabilities = ortc.getExtendedRtpCapabilities(
+		logger.debug(
+			'load() | got native RTP capabilities:%o',
+			clonedNativeRtpCapabilities
+		);
+
+		this._getSendExtendedRtpCapabilities = (
+			nativeRtpCapabilities: RtpCapabilities
+		) => {
+			return utils.clone<ExtendedRtpCapabilities>(
+				ortc.getExtendedRtpCapabilities(
+					nativeRtpCapabilities,
+					clonedRouterRtpCapabilities,
+					preferLocalCodecsOrder
+				)
+			);
+		};
+
+		const recvExtendedRtpCapabilities = ortc.getExtendedRtpCapabilities(
 			clonedNativeRtpCapabilities,
 			clonedRouterRtpCapabilities,
-			preferLocalCodecsOrder
-		);
-
-		logger.debug(
-			'load() | got extended RTP capabilities:%o',
-			this._extendedRtpCapabilities
-		);
-
-		// Check whether we can produce audio/video.
-		this._canProduceByKind.audio = ortc.canSend(
-			'audio',
-			this._extendedRtpCapabilities
-		);
-		this._canProduceByKind.video = ortc.canSend(
-			'video',
-			this._extendedRtpCapabilities
+			/* preferLocalCodecsOrder */ false
 		);
 
 		// Generate our receiving RTP capabilities for receiving media.
 		this._recvRtpCapabilities = ortc.getRecvRtpCapabilities(
-			this._extendedRtpCapabilities
+			recvExtendedRtpCapabilities
 		);
 
 		// This may throw.
@@ -330,16 +321,26 @@ export class Device {
 			this._recvRtpCapabilities
 		);
 
+		// Check whether we can produce audio/video.
+		this._canProduceByKind.audio = ortc.canSend(
+			'audio',
+			this._recvRtpCapabilities
+		);
+		this._canProduceByKind.video = ortc.canSend(
+			'video',
+			this._recvRtpCapabilities
+		);
+
 		// Generate our SCTP capabilities.
 		this._sctpCapabilities = await getNativeSctpCapabilities();
+
+		// This may throw.
+		ortc.validateSctpCapabilities(this._sctpCapabilities);
 
 		logger.debug(
 			'load() | got native SCTP capabilities:%o',
 			this._sctpCapabilities
 		);
-
-		// This may throw.
-		ortc.validateSctpCapabilities(this._sctpCapabilities);
 
 		logger.debug('load() succeeded');
 
@@ -471,7 +472,8 @@ export class Device {
 			additionalSettings,
 			appData,
 			handlerFactory: this._handlerFactory,
-			extendedRtpCapabilities: this._extendedRtpCapabilities!,
+			getSendExtendedRtpCapabilities: this._getSendExtendedRtpCapabilities!,
+			recvRtpCapabilities: this._recvRtpCapabilities!,
 			canProduceByKind: this._canProduceByKind,
 		});
 

src/Transport.ts
@@ -14,6 +14,7 @@ import { Consumer, type ConsumerOptions } from './Consumer';
 import { DataProducer, type DataProducerOptions } from './DataProducer';
 import { DataConsumer, type DataConsumerOptions } from './DataConsumer';
 import type {
+	RtpCapabilities,
 	RtpParameters,
 	MediaKind,
 	RtpEncodingParameters,
@@ -209,8 +210,13 @@ export class Transport<
 	private _closed = false;
 	// Direction.
 	private readonly _direction: 'send' | 'recv';
-	// Extended RTP capabilities.
-	private readonly _extendedRtpCapabilities: ExtendedRtpCapabilities;
+	// Callback for sending Transports to request sending extended RTP capabilities
+	// on demand.
+	private _getSendExtendedRtpCapabilities: (
+		nativeRtpCapabilities: RtpCapabilities
+	) => ExtendedRtpCapabilities;
+	// Recv RTP capabilities.
+	private readonly _recvRtpCapabilities: RtpCapabilities;
 	// Whether we can produce audio/video based on computed extended RTP
 	// capabilities.
 	private readonly _canProduceByKind: CanProduceByKind;
@@ -268,12 +274,16 @@ export class Transport<
 		additionalSettings,
 		appData,
 		handlerFactory,
-		extendedRtpCapabilities,
+		getSendExtendedRtpCapabilities,
+		recvRtpCapabilities,
 		canProduceByKind,
 	}: {
 		direction: 'send' | 'recv';
 		handlerFactory: HandlerFactory;
-		extendedRtpCapabilities: ExtendedRtpCapabilities;
+		getSendExtendedRtpCapabilities: (
+			nativeRtpCapabilities: RtpCapabilities
+		) => ExtendedRtpCapabilities;
+		recvRtpCapabilities: RtpCapabilities;
 		canProduceByKind: CanProduceByKind;
 	} & TransportOptions<TransportAppData>) {
 		super();
@@ -282,7 +292,8 @@ export class Transport<
 
 		this._id = id;
 		this._direction = direction;
-		this._extendedRtpCapabilities = extendedRtpCapabilities;
+		this._getSendExtendedRtpCapabilities = getSendExtendedRtpCapabilities;
+		this._recvRtpCapabilities = recvRtpCapabilities;
 		this._canProduceByKind = canProduceByKind;
 		this._maxSctpMessageSize = sctpParameters
 			? sctpParameters.maxMessageSize
@@ -305,7 +316,7 @@ export class Transport<
 			iceServers,
 			iceTransportPolicy,
 			additionalSettings: clonedAdditionalSettings,
-			extendedRtpCapabilities,
+			getSendExtendedRtpCapabilities: this._getSendExtendedRtpCapabilities,
 		});
 
 		this._appData = appData ?? ({} as TransportAppData);
@@ -678,7 +689,7 @@ export class Transport<
 		// Ensure the device can consume it.
 		const canConsume = ortc.canReceive(
 			clonedRtpParameters,
-			this._extendedRtpCapabilities
+			this._recvRtpCapabilities
 		);
 
 		if (!canConsume) {

src/handlers/Chrome111.ts
@@ -2,17 +2,13 @@ import * as sdpTransform from 'sdp-transform';
 import type * as SdpTransform from 'sdp-transform';
 import { EnhancedEventEmitter } from '../enhancedEvents';
 import { Logger } from '../Logger';
-import * as utils from '../utils';
 import * as ortc from '../ortc';
 import { InvalidStateError } from '../errors';
 import { parse as parseScalabilityMode } from '../scalabilityModes';
 import type { IceParameters, DtlsRole } from '../Transport';
-import type {
-	RtpCapabilities,
-	MediaKind,
-	RtpParameters,
-} from '../RtpParameters';
+import type { RtpCapabilities, MediaKind } from '../RtpParameters';
 import type { SctpCapabilities, SctpStreamParameters } from '../SctpParameters';
+import type { ExtendedRtpCapabilities } from '../privateTypes';
 import * as sdpCommonUtils from './sdp/commonUtils';
 import * as sdpUnifiedPlanUtils from './sdp/unifiedPlanUtils';
 import * as ortcUtils from './ortc/utils';
@@ -47,13 +43,10 @@ export class Chrome111
 	private _direction: 'send' | 'recv';
 	// Remote SDP handler.
 	private _remoteSdp: RemoteSdp;
-	// Generic sending RTP parameters for audio and video.
-	private _sendingRtpParametersByKind: { [K in MediaKind]: RtpParameters };
-	// Generic sending RTP parameters for audio and video suitable for the SDP
-	// remote answer.
-	private _sendingRemoteRtpParametersByKind: {
-		[K in MediaKind]: RtpParameters;
-	};
+	// Callback to request sending extended RTP capabilities on demand.
+	private _getSendExtendedRtpCapabilities: (
+		nativeRtpCapabilities: RtpCapabilities
+	) => ExtendedRtpCapabilities;
 	// Initial server side DTLS role. If not 'auto', it will force the opposite
 	// value in client side.
 	private _forcedLocalDtlsRole?: DtlsRole;
@@ -105,12 +98,8 @@ export class Chrome111
 					pc = undefined;
 
 					const sdpObject = sdpTransform.parse(offer.sdp!);
-					const nativeRtpCapabilities = sdpCommonUtils.extractRtpCapabilities({
-						sdpObject,
-					});
-
-					// libwebrtc supports NACK for OPUS but doesn't announce it.
-					ortcUtils.addNackSupportForOpus(nativeRtpCapabilities);
+					const nativeRtpCapabilities =
+						Chrome111.getLocalRtpCapabilities(sdpObject);
 
 					return nativeRtpCapabilities;
 				} catch (error) {
@@ -133,6 +122,19 @@ export class Chrome111
 		};
 	}
 
+	private static getLocalRtpCapabilities(
+		localSdpObject: SdpTransform.SessionDescription
+	): RtpCapabilities {
+		const nativeRtpCapabilities = sdpCommonUtils.extractRtpCapabilities({
+			sdpObject: localSdpObject,
+		});
+
+		// libwebrtc supports NACK for OPUS but doesn't announce it.
+		ortcUtils.addNackSupportForOpus(nativeRtpCapabilities);
+
+		return nativeRtpCapabilities;
+	}
+
 	private constructor({
 		direction,
 		iceParameters,
@@ -142,7 +144,7 @@ export class Chrome111
 		iceServers,
 		iceTransportPolicy,
 		additionalSettings,
-		extendedRtpCapabilities,
+		getSendExtendedRtpCapabilities,
 	}: HandlerOptions) {
 		super();
 
@@ -157,21 +159,7 @@ export class Chrome111
 			sctpParameters,
 		});
 
-		this._sendingRtpParametersByKind = {
-			audio: ortc.getSendingRtpParameters('audio', extendedRtpCapabilities),
-			video: ortc.getSendingRtpParameters('video', extendedRtpCapabilities),
-		};
-
-		this._sendingRemoteRtpParametersByKind = {
-			audio: ortc.getSendingRemoteRtpParameters(
-				'audio',
-				extendedRtpCapabilities
-			),
-			video: ortc.getSendingRemoteRtpParameters(
-				'video',
-				extendedRtpCapabilities
-			),
-		};
+		this._getSendExtendedRtpCapabilities = getSendExtendedRtpCapabilities;
 
 		if (dtlsParameters.role && dtlsParameters.role !== 'auto') {
 			this._forcedLocalDtlsRole =
@@ -362,27 +350,6 @@ export class Chrome111
 			});
 		}
 
-		const sendingRtpParameters: RtpParameters = utils.clone<RtpParameters>(
-			this._sendingRtpParametersByKind[track.kind as MediaKind]
-		);
-
-		// This may throw.
-		sendingRtpParameters.codecs = ortc.reduceCodecs(
-			sendingRtpParameters.codecs,
-			codec
-		);
-
-		const sendingRemoteRtpParameters: RtpParameters =
-			utils.clone<RtpParameters>(
-				this._sendingRemoteRtpParametersByKind[track.kind as MediaKind]
-			);
-
-		// This may throw.
-		sendingRemoteRtpParameters.codecs = ortc.reduceCodecs(
-			sendingRemoteRtpParameters.codecs,
-			codec
-		);
-
 		const mediaSectionIdx = this._remoteSdp.getNextMediaSectionIdx();
 		const transceiver = this._pc.addTransceiver(track, {
 			direction: 'sendonly',
@@ -400,6 +367,36 @@ export class Chrome111
 		if (localSdpObject.extmapAllowMixed) {
 			this._remoteSdp.setSessionExtmapAllowMixed();
 		}
+
+		const nativeRtpCapabilities =
+			Chrome111.getLocalRtpCapabilities(localSdpObject);
+		const sendExtendedRtpCapabilities = this._getSendExtendedRtpCapabilities(
+			nativeRtpCapabilities
+		);
+
+		// Generic sending RTP parameters.
+		const sendingRtpParameters = ortc.getSendingRtpParameters(
+			track.kind as MediaKind,
+			sendExtendedRtpCapabilities
+		);
+
+		// This may throw.
+		sendingRtpParameters.codecs = ortc.reduceCodecs(
+			sendingRtpParameters.codecs,
+			codec
+		);
+
+		// Generic sending RTP parameters suitable for the SDP remote answer.
+		const sendingRemoteRtpParameters = ortc.getSendingRemoteRtpParameters(
+			track.kind as MediaKind,
+			sendExtendedRtpCapabilities
+		);
+
+		// This may throw.
+		sendingRemoteRtpParameters.codecs = ortc.reduceCodecs(
+			sendingRemoteRtpParameters.codecs,
+			codec
+		);
 
 		if (!this._transportReady) {
 			await this.setupTransport({

commit a30cd214ce90e301d81f9d47eb2f18789296ef2a

Fix native RTP capabilities not being normalized
## Details

- Fixes #340
- We need to normalize locally generated native RTP capabilities to be able to compare them properly with Router's RTP capabilities.
- This was a regression introduced in PR #336.

src/Device.ts
@@ -276,7 +276,7 @@ export class Device {
 		);
 
 		// This may throw.
-		ortc.validateRtpCapabilities(clonedRouterRtpCapabilities);
+		ortc.validateAndNormalizeRtpCapabilities(clonedRouterRtpCapabilities);
 
 		const { getNativeRtpCapabilities, getNativeSctpCapabilities } =
 			this._handlerFactory;
@@ -286,7 +286,7 @@ export class Device {
 		);
 
 		// This may throw.
-		ortc.validateRtpCapabilities(clonedNativeRtpCapabilities);
+		ortc.validateAndNormalizeRtpCapabilities(clonedNativeRtpCapabilities);
 
 		logger.debug(
 			'load() | got native RTP capabilities:%o',
@@ -317,7 +317,7 @@ export class Device {
 		);
 
 		// This may throw.
-		ortc.validateRtpCapabilities(this._recvRtpCapabilities);
+		ortc.validateAndNormalizeRtpCapabilities(this._recvRtpCapabilities);
 
 		logger.debug(
 			'load() | got receiving RTP capabilities:%o',

src/Transport.ts
@@ -594,7 +594,7 @@ export class Transport<
 
 					try {
 						// This will fill rtpParameters's missing fields with default values.
-						ortc.validateRtpParameters(rtpParameters);
+						ortc.validateAndNormalizeRtpParameters(rtpParameters);
 
 						const { id } = await new Promise<{ id: string }>(
 							(resolve, reject) => {
@@ -771,7 +771,7 @@ export class Transport<
 				});
 
 			// This will fill sctpStreamParameters's missing fields with default values.
-			ortc.validateSctpStreamParameters(sctpStreamParameters);
+			ortc.validateAndNormalizeSctpStreamParameters(sctpStreamParameters);
 
 			const { id } = await new Promise<{ id: string }>((resolve, reject) => {
 				this.safeEmit(
@@ -842,7 +842,7 @@ export class Transport<
 		const clonedSctpStreamParameters = utils.clone(sctpStreamParameters);
 
 		// This may throw.
-		ortc.validateSctpStreamParameters(clonedSctpStreamParameters);
+		ortc.validateAndNormalizeSctpStreamParameters(clonedSctpStreamParameters);
 
 		// Enqueue command.
 		return this._awaitQueue.push(async () => {

src/handlers/Chrome111.ts
@@ -132,6 +132,9 @@ export class Chrome111
 			sdpObject: localSdpObject,
 		});
 
+		// Need to validate and normalize native RTP capabilities.
+		ortc.validateAndNormalizeRtpCapabilities(nativeRtpCapabilities);
+
 		// libwebrtc supports NACK for OPUS but doesn't announce it.
 		ortcUtils.addNackSupportForOpus(nativeRtpCapabilities);
 

src/handlers/Chrome74.ts
@@ -129,6 +129,9 @@ export class Chrome74
 			sdpObject: localSdpObject,
 		});
 
+		// Need to validate and normalize native RTP capabilities.
+		ortc.validateAndNormalizeRtpCapabilities(nativeRtpCapabilities);
+
 		// libwebrtc supports NACK for OPUS but doesn't announce it.
 		ortcUtils.addNackSupportForOpus(nativeRtpCapabilities);
 

src/handlers/FakeHandler.ts
@@ -101,6 +101,9 @@ export class FakeHandler
 		const nativeRtpCapabilities =
 			fakeParameters.generateNativeRtpCapabilities();
 
+		// Need to validate and normalize native RTP capabilities.
+		ortc.validateAndNormalizeRtpCapabilities(nativeRtpCapabilities);
+
 		return nativeRtpCapabilities;
 	}
 

src/handlers/Firefox120.ts
@@ -158,6 +158,9 @@ export class Firefox120
 			sdpObject: localSdpObject,
 		});
 
+		// Need to validate and normalize native RTP capabilities.
+		ortc.validateAndNormalizeRtpCapabilities(nativeRtpCapabilities);
+
 		return nativeRtpCapabilities;
 	}
 

src/handlers/ReactNative106.ts
@@ -130,6 +130,9 @@ export class ReactNative106
 			sdpObject: localSdpObject,
 		});
 
+		// Need to validate and normalize native RTP capabilities.
+		ortc.validateAndNormalizeRtpCapabilities(nativeRtpCapabilities);
+
 		// libwebrtc supports NACK for OPUS but doesn't announce it.
 		ortcUtils.addNackSupportForOpus(nativeRtpCapabilities);
 

src/handlers/Safari12.ts
@@ -128,6 +128,9 @@ export class Safari12
 			sdpObject: localSdpObject,
 		});
 
+		// Need to validate and normalize native RTP capabilities.
+		ortc.validateAndNormalizeRtpCapabilities(nativeRtpCapabilities);
+
 		// libwebrtc supports NACK for OPUS but doesn't announce it.
 		ortcUtils.addNackSupportForOpus(nativeRtpCapabilities);
 

src/ortc.ts
@@ -30,7 +30,9 @@ const RTP_PROBATOR_CODEC_PAYLOAD_TYPE = 127;
  * fields with default values.
  * It throws if invalid.
  */
-export function validateRtpCapabilities(caps: RtpCapabilities): void {
+export function validateAndNormalizeRtpCapabilities(
+	caps: RtpCapabilities
+): void {
 	if (typeof caps !== 'object') {
 		throw new TypeError('caps is not an object');
 	}
@@ -43,7 +45,7 @@ export function validateRtpCapabilities(caps: RtpCapabilities): void {
 	}
 
 	for (const codec of caps.codecs) {
-		validateRtpCodecCapability(codec);
+		validateAndNormalizeRtpCodecCapability(codec);
 	}
 
 	// headerExtensions is optional. If unset, fill with an empty array.
@@ -54,7 +56,7 @@ export function validateRtpCapabilities(caps: RtpCapabilities): void {
 	}
 
 	for (const ext of caps.headerExtensions) {
-		validateRtpHeaderExtension(ext);
+		validateAndNormalizeRtpHeaderExtension(ext);
 	}
 }
 
@@ -63,7 +65,7 @@ export function validateRtpCapabilities(caps: RtpCapabilities): void {
  * fields with default values.
  * It throws if invalid.
  */
-export function validateRtpParameters(params: RtpParameters): void {
+export function validateAndNormalizeRtpParameters(params: RtpParameters): void {
 	if (typeof params !== 'object') {
 		throw new TypeError('params is not an object');
 	}
@@ -79,7 +81,7 @@ export function validateRtpParameters(params: RtpParameters): void {
 	}
 
 	for (const codec of params.codecs) {
-		validateRtpCodecParameters(codec);
+		validateAndNormalizeRtpCodecParameters(codec);
 	}
 
 	// headerExtensions is optional. If unset, fill with an empty array.
@@ -101,7 +103,7 @@ export function validateRtpParameters(params: RtpParameters): void {
 	}
 
 	for (const encoding of params.encodings) {
-		validateRtpEncodingParameters(encoding);
+		validateAndNormalizeRtpEncodingParameters(encoding);
 	}
 
 	// rtcp is optional. If unset, fill with an empty object.
@@ -111,7 +113,7 @@ export function validateRtpParameters(params: RtpParameters): void {
 		params.rtcp = {};
 	}
 
-	validateRtcpParameters(params.rtcp);
+	validateAndNormalizeRtcpParameters(params.rtcp);
 }
 
 /**
@@ -119,7 +121,7 @@ export function validateRtpParameters(params: RtpParameters): void {
  * fields with default values.
  * It throws if invalid.
  */
-export function validateSctpStreamParameters(
+export function validateAndNormalizeSctpStreamParameters(
 	params: SctpStreamParameters
 ): void {
 	if (typeof params !== 'object') {
@@ -658,7 +660,7 @@ export function generateProbatorRtpParameters(
 	videoRtpParameters = utils.clone<RtpParameters>(videoRtpParameters);
 
 	// This may throw.
-	validateRtpParameters(videoRtpParameters);
+	validateAndNormalizeRtpParameters(videoRtpParameters);
 
 	const rtpParameters: RtpParameters = {
 		mid: RTP_PROBATOR_MID,
@@ -694,7 +696,7 @@ export function canReceive(
 	rtpCapabilities: RtpCapabilities
 ): boolean {
 	// This may throw.
-	validateRtpParameters(rtpParameters);
+	validateAndNormalizeRtpParameters(rtpParameters);
 
 	if (rtpParameters.codecs.length === 0) {
 		return false;
@@ -712,7 +714,9 @@ export function canReceive(
  * fields with default values.
  * It throws if invalid.
  */
-function validateRtpCodecCapability(codec: RtpCodecCapability): void {
+function validateAndNormalizeRtpCodecCapability(
+	codec: RtpCodecCapability
+): void {
 	const MimeTypeRegex = new RegExp('^(audio|video)/(.+)', 'i');
 
 	if (typeof codec !== 'object') {
@@ -785,7 +789,7 @@ function validateRtpCodecCapability(codec: RtpCodecCapability): void {
 	}
 
 	for (const fb of codec.rtcpFeedback) {
-		validateRtcpFeedback(fb);
+		validateAndNormalizeRtcpFeedback(fb);
 	}
 }
 
@@ -794,7 +798,7 @@ function validateRtpCodecCapability(codec: RtpCodecCapability): void {
  * fields with default values.
  * It throws if invalid.
  */
-function validateRtcpFeedback(fb: RtcpFeedback): void {
+function validateAndNormalizeRtcpFeedback(fb: RtcpFeedback): void {
 	if (typeof fb !== 'object') {
 		throw new TypeError('fb is not an object');
 	}
@@ -815,7 +819,7 @@ function validateRtcpFeedback(fb: RtcpFeedback): void {
  * fields with default values.
  * It throws if invalid.
  */
-function validateRtpHeaderExtension(ext: RtpHeaderExtension): void {
+function validateAndNormalizeRtpHeaderExtension(ext: RtpHeaderExtension): void {
 	if (typeof ext !== 'object') {
 		throw new TypeError('ext is not an object');
 	}
@@ -855,7 +859,9 @@ function validateRtpHeaderExtension(ext: RtpHeaderExtension): void {
  * fields with default values.
  * It throws if invalid.
  */
-function validateRtpCodecParameters(codec: RtpCodecParameters): void {
+function validateAndNormalizeRtpCodecParameters(
+	codec: RtpCodecParameters
+): void {
 	const MimeTypeRegex = new RegExp('^(audio|video)/(.+)', 'i');
 
 	if (typeof codec !== 'object') {
@@ -927,7 +933,7 @@ function validateRtpCodecParameters(codec: RtpCodecParameters): void {
 	}
 
 	for (const fb of codec.rtcpFeedback) {
-		validateRtcpFeedback(fb);
+		validateAndNormalizeRtcpFeedback(fb);
 	}
 }
 
@@ -984,7 +990,9 @@ function validateRtpHeaderExtensionParameters(
  * fields with default values.
  * It throws if invalid.
  */
-function validateRtpEncodingParameters(encoding: RtpEncodingParameters): void {
+function validateAndNormalizeRtpEncodingParameters(
+	encoding: RtpEncodingParameters
+): void {
 	if (typeof encoding !== 'object') {
 		throw new TypeError('encoding is not an object');
 	}
@@ -1028,7 +1036,7 @@ function validateRtpEncodingParameters(encoding: RtpEncodingParameters): void {
  * fields with default values.
  * It throws if invalid.
  */
-function validateRtcpParameters(rtcp: RtcpParameters): void {
+function validateAndNormalizeRtcpParameters(rtcp: RtcpParameters): void {
 	if (typeof rtcp !== 'object') {
 		throw new TypeError('rtcp is not an object');
 	}

src/test/fakeParameters.ts
@@ -184,8 +184,11 @@ export function generateRouterRtpCapabilities(): mediasoupClient.types.RtpCapabi
 	});
 }
 
+// NOTE: We don't freeze these RTP capabilities because we do need to normalize
+// them as we do in real browser handlers (this is an object supposed to be
+// generated internally so it's ok).
 export function generateNativeRtpCapabilities(): mediasoupClient.types.RtpCapabilities {
-	return utils.deepFreeze<mediasoupClient.types.RtpCapabilities>({
+	return {
 		codecs: [
 			{
 				mimeType: 'audio/opus',
@@ -361,7 +364,7 @@ export function generateNativeRtpCapabilities(): mediasoupClient.types.RtpCapabi
 				preferredId: 10,
 			},
 		],
-	});
+	};
 }
 
 export function generateNativeSctpCapabilities(): mediasoupClient.types.SctpCapabilities {