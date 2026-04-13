cfee55fdfdad4379b86f2d51db053a857c98b2b2
Pontus Fagerström <pontus.fagerstrom@whereby.com>
2026-04-13 07:03:15 +0200

    commit lib files

---
+++ lib/test/uaTestCases.d.ts
@@ -0,0 +1,9 @@
+import type { BuiltinHandlerName } from '../Device';
+type UATestCase = {
+    desc: string;
+    ua: string;
+    expect?: BuiltinHandlerName;
+};
+export declare const uaTestCases: UATestCase[];
+export {};
+//# sourceMappingURL=uaTestCases.d.ts.map
