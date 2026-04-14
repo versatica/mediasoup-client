b1b681e281193dbf3639f3780a69a84e46b65b91
Pontus Fagerström <144339208+pnts-se-whereby@users.noreply.github.com>
2026-04-14 08:46:01 +0200

    remove lib files (#4)

--- lib/test/test.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This test runs in Node so no browser auto-detection is done. Instead, a
- * FakeHandler device is used.
- */
-export {};
-//# sourceMappingURL=test.d.ts.map
--- lib/test/uaTestCases.d.ts
+++
@@ -1,9 +0,0 @@
-import type { BuiltinHandlerName } from '../Device';
-type UATestCase = {
-    desc: string;
-    ua: string;
-    expect?: BuiltinHandlerName;
-};
-export declare const uaTestCases: UATestCase[];
-export {};
-//# sourceMappingURL=uaTestCases.d.ts.map
